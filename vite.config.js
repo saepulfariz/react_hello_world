c00357b99290de45fa8000debed35a50ea1a71d7
saepulfariz <saepulfariz108@gmail.com>
2024-07-09 08:40:28 +0700

    add base url build

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/hello/'
+  // https://vite-plugin-ssr.com/base-url
 })
